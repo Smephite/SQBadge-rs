3d338741e6a04eb745d6fca7ac8fdb88a7d00a9f
Smephite <smephite@yahoo.com>
2021-08-14 15:06:13 +0000

    Deploying to gh-pages from @ Smephite/SQBadge-rs@a103f231ec3447ae711ddcc0609896c3b501d240 🚀

--- index-c7bffead84fe93f8.js
+++ index-c7bffead84fe93f8.js
@@ -286,7 +286,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b58235e991b8fc9b_bg.wasm', import.meta.url);
+        input = new URL('index-c7bffead84fe93f8_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
