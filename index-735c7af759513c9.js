c2097f9636f384ebe7873300b0b663f50ff01878
Smephite <smephite@yahoo.com>
2021-10-24 21:32:53 +0000

    Deploying to gh-pages from @ Smephite/SQBadge-rs@75acff942eec8be73f9832c8647fdc599b09b218 🚀

--- index-735c7af759513c9.js
+++ index-735c7af759513c9.js
@@ -286,7 +286,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-4d703ae86a5c34a2_bg.wasm', import.meta.url);
+        input = new URL('index-735c7af759513c9_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
